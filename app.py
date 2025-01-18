76292414c1f4082fe14ee0941c79fc4b6270f47f
Ngwaru <162811934+Ngwaru@users.noreply.github.com>
2025-01-19 00:46:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -11,21 +11,46 @@ display_message = ""
 
 model = models.load_model("cxr_normal_tb_vgg16_model.keras")
 
-def predict_image(model, path_to_image):
+model_xray_or_not = models.load_model("cxr_or_not_vgg16_model.keras")
+
+
+def is_xray(model, path_to_image):
     image = Image.open(path_to_image)
-    image = image.convert("RGB")
-    image = image.resize((200, 200))
+    image =image.convert("RGB")
+    image = image.resize((156,156))
     data = np.asarray(image)
     data = data/255
-    prob_of_tb = model.predict(np.array([data]))
-   
-    if prob_of_tb > 0.5:
-        message = "TB is likely"
-    elif prob_of_tb > 0.4:
-        message = "TB cannot be ruled out"
+    xray_or_not = model.predict(np.array([data]))
+    print(xray_or_not)
+    if xray_or_not > 0.5:
+        return True
     else:
-        message = "TB is unlikely"
-    return prob_of_tb, message
+        return False
+
+
+
+
+def predict_image(model, path_to_image):
+    is_it_an_xray = is_xray(model_xray_or_not, path_to_image)
+    if is_it_an_xray:
+        image = Image.open(path_to_image)
+        image = image.convert("RGB")
+        image = image.resize((200, 200))
+        data = np.asarray(image)
+        data = data/255
+        prob_of_tb = model.predict(np.array([data]))
+        print(prob_of_tb)
+        if prob_of_tb > 0.5:
+            message = "TB is likely"
+        elif prob_of_tb > 0.4:
+            message = "TB cannot be ruled out"
+        else:
+            message = "TB is unlikely"
+        return prob_of_tb, message
+    else:
+        prob_of_tb = np.array([[0.0]])
+        message = "This is not an X-ray"
+        return prob_of_tb, message
 
 
 index = """
@@ -34,7 +59,7 @@ index = """
 
 Upload a CXR to review
 
-<|{content}|file_selector|extensions= .png,.jpg|>
+<|{content}|file_selector|extensions= .png,.jpg, .jpeg|>
 
 
 
