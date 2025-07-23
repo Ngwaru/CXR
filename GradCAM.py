b9d161f9c3ea71d526677d4e6cd1f618c668435a
Ngwaru Munodawafa <munodawafan@gmail.com>
2025-07-23 14:02:50 +0200

    removed def main and if __name__ == '__main__' from the GradCAM file

--- GradCAM.py
+++ GradCAM.py
@@ -7,76 +7,72 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
+preprocess_input = keras.applications.xception.preprocess_input
+
+class Grad_CAM_class():
+    def __init__(self, model, last_conv_layer_name, img_path, classes_names, size):
+        self.model = model
+        self.last_conv_layer_name = last_conv_layer_name
+        # self.img_not_changed = keras.utils.array_to_img(img)
+        self.img_path = img_path
+        self.classes_names = classes_names
+        self.size = size
+        self.img_array = None
 
 
-def main():
-    preprocess_input = keras.applications.xception.preprocess_input
-    class Grad_CAM_class():
-        def __init__(self, model, last_conv_layer_name, img_path, classes_names, size):
-            self.model = model
-            self.last_conv_layer_name = last_conv_layer_name
-            # self.img_not_changed = keras.utils.array_to_img(img)
-            self.img_path = img_path
-            self.classes_names = classes_names
-            self.size = size
-            self.img_array = None
+    def decode_predictions(self, preds):
+        return self.classes_names[round(preds[0][0])]
+    
 
+    def get_img_array(self): 
+        self.img = keras.utils.load_img(self.img_path, target_size=self.size)
+        self.img_array = keras.utils.img_to_array(self.img)
+        self.img_array  = np.expand_dims(self.img, axis=0)
+        return self.img_array 
+    
+    def make_gradcam_heatmap(self):
+        grad_model = keras.models.Model(
+            self.model.inputs, [self.model.get_layer(self.last_conv_layer_name).output, self.model.output]
+        )
+        pred_index = None
 
-        def decode_predictions(self, preds):
-            return self.classes_names[round(preds[0][0])]
-        
+        with tf.GradientTape() as tape:
+            # self.img = get_img_array(self)
+            last_conv_layer_output, preds = grad_model(preprocess_input(self.img_array))
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
 
-        def get_img_array(self): 
-            self.img = keras.utils.load_img(self.img_path, target_size=self.size)
-            self.img_array = keras.utils.img_to_array(self.img)
-            self.img_array  = np.expand_dims(self.img, axis=0)
-            return self.img_array 
-        
-        def make_gradcam_heatmap(self):
-            grad_model = keras.models.Model(
-                self.model.inputs, [self.model.get_layer(self.last_conv_layer_name).output, self.model.output]
-            )
-            pred_index = None
+        grads = tape.gradient(class_channel, last_conv_layer_output)
 
-            with tf.GradientTape() as tape:
-                # self.img = get_img_array(self)
-                last_conv_layer_output, preds = grad_model(preprocess_input(self.img_array))
-                if pred_index is None:
-                    pred_index = tf.argmax(preds[0])
-                class_channel = preds[:, pred_index]
+        pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+        last_conv_layer_output =last_conv_layer_output[0]
 
-            grads = tape.gradient(class_channel, last_conv_layer_output)
+        self.heatmap = last_conv_layer_output@pooled_grads[..., tf.newaxis]
+        self.heatmap = tf.squeeze(self.heatmap)
 
-            pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
-            last_conv_layer_output =last_conv_layer_output[0]
+        self.heatmap = tf.maximum(self.heatmap, 0)/tf.math.reduce_max(self.heatmap)
 
-            self.heatmap = last_conv_layer_output@pooled_grads[..., tf.newaxis]
-            self.heatmap = tf.squeeze(self.heatmap)
+        return self.heatmap.numpy()
+    
 
-            self.heatmap = tf.maximum(self.heatmap, 0)/tf.math.reduce_max(self.heatmap)
+    def save_and_display_gracam(self):
+        cam_path = "cam.jpg"
+        alpha = 0.4
+        self.new_heatmap = np.uint8(255*self.heatmap)
 
-            return self.heatmap.numpy()
-        
+        jet = mpl.colormaps["jet"]
+        jet_colors = jet(np.arange(256))[:, :3]
 
-        def save_and_display_gracam(self):
-            cam_path = "cam.jpg"
-            alpha = 0.4
-            self.new_heatmap = np.uint8(255*self.heatmap)
+        jet_heatmap = jet_colors[self.new_heatmap]
+        jet_heatmap = keras.utils.array_to_img(jet_heatmap)
+        jet_heatmap = jet_heatmap.resize((self.img_array.shape[1], self.img_array.shape[0]), resample=Image.Resampling.LANCZOS)
+        jet_heatmap = keras.utils.img_to_array(jet_heatmap)
 
-            jet = mpl.colormaps["jet"]
-            jet_colors = jet(np.arange(256))[:, :3]
+        superimposed_img = jet_heatmap*alpha*self.img
+        superimposed_img = keras.utils.array_to_img(superimposed_img)
+        superimposed_img.save(cam_path)
+        new_image = Image.open(cam_path)
 
-            jet_heatmap = jet_colors[self.new_heatmap]
-            jet_heatmap = keras.utils.array_to_img(jet_heatmap)
-            jet_heatmap = jet_heatmap.resize((self.img_array.shape[1], self.img_array.shape[0]), resample=Image.Resampling.LANCZOS)
-            jet_heatmap = keras.utils.img_to_array(jet_heatmap)
+        return new_image
 
-            superimposed_img = jet_heatmap*alpha*self.img
-            superimposed_img = keras.utils.array_to_img(superimposed_img)
-            superimposed_img.save(cam_path)
-            new_image = Image.open(cam_path)
-
-            return new_image
-
-if __name__ == "__main__":
-    main()
